ee7a9f56f5716a25e7a49d90f870a0dad14d90a6
NataliyaChe <happylibrarygeek@gmail.com>
2023-02-16 23:43:13 +0200

    spoiler fixed

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 function Post({post, onclickHandler}) {
-    return (
-        <li className='post-item'>   
-            <p>{post.text}</p>
-            <p>{post.date}</p>
-            <p>{post.id}</p>
-            <button 
-                className='button' 
-                onClick={onclickHandler} 
-                data-id={post.id}>
-                Like
-            </button>
-            <span className='likes'>{post.likes}</span>   
-        </li>
-    );
+  const spoiler = useRef();
+  const [newClass, setNewClass] = useState(true)
+  const onclickSpoilerHandler = (event) => {
+
+    newClass === false ? setNewClass(true) : setNewClass(false)
+    
+    // if(post.id === +spoilerId) {
+    //   // spoiler.current.className('active')
+    //   setNewClass(false)
+    // }
+    
+}
+
+  return (
+    <li className='post-item'>   
+      <p>{post.text}</p>
+      <button 
+        className='button' 
+        onClick={onclickSpoilerHandler}
+        data-id={post.id}>
+              Spoiler
+      </button>
+      <div className={newClass ? 'hide' : 'show'} ref={spoiler}>
+        <p>{post.date}</p>
+        <button 
+          className='button' 
+          onClick={onclickHandler} 
+          data-id={post.id}>
+          Like
+        </button>
+        <span className='likes'>{post.likes}</span>  
+      </div>
+    </li>
+  );
 }
 
 export default Post;
