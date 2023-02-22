e42514d87d2f24902824b7a406ce908d6f08edc3
NataliyaChe <happylibrarygeek@gmail.com>
2023-02-22 21:08:43 +0200

    get added

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,9 +5,9 @@ function Blog({posts, onclickHandler}) {
     return (
         <div className='blog-container'>
             <ul className='posts'>
-                {/* {posts.map(post => {
+                {posts.map(post => {
                     return <Post post={post} key={post.id} onclickHandler={onclickHandler}/>
-                })} */}
+                })}
             </ul>     
         </div>
     );
--- src/components/Main.js
+++ src/components/Main.js
@@ -9,75 +9,26 @@ function Main() {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
+        console.log('posts.length', posts.length);
         const fetchPosts = async () => {
 
           const data = await fetch(`http://localhost:3004/posts`)
-          console.log('data', data.json());
           const posts = await data.json();
-          setPosts(posts.data)
+          setPosts(posts)
         }
         fetchPosts()
-      }, []);
-    // const [posts, setPosts] = useState([
-    //     {
-    //         text: 'first post',
-    //         date: new Date(2023, 0, 2, 1, 10, 6),
-    //         id: 1672614606000,
-    //         likes: 0,
-    //     },
-    //     {
-    //         text: 'second post',
-    //         date: new Date(2023, 0, 15, 11, 10, 7),
-    //         id: 1673773807000,
-    //         likes: 0,
-    //     },
-    //     {
-    //         text: 'third post',
-    //         date: new Date(2023, 0, 25, 11, 10, 10),
-    //         id: 1674637810000,
-    //         likes: 0,
-    //     },
-    //     {
-    //         text: 'fourth post',
-    //         date: new Date(2023, 0, 30, 11, 10, 2),
-    //         id: 1675069802000,
-    //         likes: 0,
-    //     },
-    //     {
-    //         text: 'fifth post',
-    //         date: new Date(2023, 1, 2, 11, 10, 3),
-    //         id: 1675329003000,
-    //         likes: 0,
-    //     },
-    //     {
-    //         text: 'sixth post',
-    //         date: new Date(2023, 1, 6, 11, 10, 15),
-    //         id: 1675674615000,
-    //         likes: 0,
-    //     },
-    //     {
-    //         text: 'seventh post',
-    //         date: new Date(2023, 1, 10, 11, 10, 20),
-    //         id: 1676020220000,
-    //         likes: 0,
-    //     },
-    //     {
-    //         text: 'eighth post',
-    //         date: new Date(2023, 1, 18, 11, 10, 8),
-    //         id: 1676711408000,
-    //         likes: 0,
-    //     },
-    // ]);
+      }, [posts.length]);
+      console.log('data', posts);
 
     const postsPerPage = 5;
     const [firstPost, setFirstPost] = useState(0)
     const lastPost = firstPost + postsPerPage;
 
-    // const paginatedPosts = (posts.sort((a, b) => (
-    //     b.likes - a.likes || b.date - a.date
-    // )).slice(firstPost, lastPost));
+    const paginatedPosts = (posts.sort((a, b) => (
+        b.likes - a.likes || b.date - a.date
+    )).slice(firstPost, lastPost));
 
-    // const totalPages = Math.ceil(posts.length / postsPerPage);
+    const totalPages = Math.ceil(posts.length / postsPerPage);
 
     const pageChangeHandler = (event) => {
         setFirstPost(event.selected * postsPerPage)  
@@ -95,6 +46,16 @@ function Main() {
             ]
         )
     }
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const data = await fetch(`http://localhost:3004/posts`, {
+                method: 'POST',
+                body: JSON.stringify(posts)
+            })
+        }
+        fetchPosts()
+    }, [posts])
 
     const onclickHandler = (event) => {
         const buttonId = event.target.dataset.id;
@@ -114,7 +75,7 @@ function Main() {
                 <Form onCreate={addPost}/>
                 <DatePicker posts={posts} setPosts={setPosts} allPosts={allPosts} setAllPosts={setAllPosts}/>
             </div>
-            {/* {posts.length > 5 &&    
+            {posts.length > 5 &&    
             <ReactPaginate
                 breakLabel="..."
                 nextLabel="next >"
@@ -124,12 +85,10 @@ function Main() {
                 previousLabel="< previous"
                 renderOnZeroPageCount={null}
             />
-            } */}
+            }
             <Blog 
-                // posts={posts.length > 5 ? paginatedPosts : posts} 
-                // onclickHandler={onclickHandler}/>
-                // posts={posts.length > 5 ? paginatedPosts : posts} onclickHandler={onclickHandler}
-                />
+                posts={posts.length > 5 ? paginatedPosts : posts} 
+                onclickHandler={onclickHandler}/>
         </div>
     );
 }
